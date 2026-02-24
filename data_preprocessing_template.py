aa4140044161e5570be59a44d2855dec24adf6ed
pvrPranavRathi <pranavrathipvr@gmailcom>
2026-02-25 01:22:32 +0530

    ML-01: Learning Data Preprocessing

--- data_preprocessing_template.py
+++ data_preprocessing_template.py
@@ -10,16 +10,50 @@ from pathlib import Path
 # Importing the dataset
 file_path = Path(r"YOUR_LOC")
 dataset = pd.read_csv(file_path)
-x = dataset.iloc[:, :-1].values
+X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
-print(x)
+print(X)
 print(y)
 
 # Taking care of missing data
 from sklearn.impute import SimpleImputer
-imputer = SimpleImputer(missing_values = np.nan, strategy = 'mean')
-imputer.fit(x[:, 1:3])
-x[:, 1:3] = imputer.transform(x[:, 1:3])
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
 
-print(x) 
+print(X)
+
+# Encoding categorical data
+
+# Encoding the independent variable
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
+
+print(X)
+
+# Encoding the Dependent variable
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+print(y)
+
+# Splitting the dataset into Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+print(X_train)
+print(y_train)
+print(X_test)
+print(y_test)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+X_train[:, 3:] = scaler.fit_transform(X_train[:, 3:])
+X_test[:, 3:] = scaler.transform(X_test[:, 3:])
+
+print(X_train)
+print(X_test)
